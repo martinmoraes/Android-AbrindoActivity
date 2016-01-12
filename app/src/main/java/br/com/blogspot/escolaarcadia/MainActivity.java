fe5c0b652ee13c28b511cbf17319f4a1edf9ab22
Martin Morães <martin.moraes@gmail.com>
2016-01-12 13:22:27 -0200

    Ponto de partida. Tudo zerado com imagens no mipmap.

---
+++ app/src/main/java/br/com/blogspot/escolaarcadia/MainActivity.java
@@ -0,0 +1,22 @@
+package br.com.blogspot.escolaarcadia;
+
+/*
+prof. Martin Morães
+http://lattes.cnpq.br/2746554350570335
+http://escolaarcadia.blogspot.com.br/
+ */
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.escolaarcadia.android_abrindoactivity.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
