12c1497f7d8ac26e59094c627998f375a62ccc05
Martin Morães <martin.moraes@gmail.com>
2016-01-12 15:13:53 -0200

    Abrindo activity do projeto e abrindo um navegador.

--- app/src/main/java/br/com/blogspot/escolaarcadia/MainActivity.java
+++ app/src/main/java/br/com/blogspot/escolaarcadia/MainActivity.java
@@ -4,11 +4,17 @@ package br.com.blogspot.escolaarcadia;
 prof. Martin Morães
 http://lattes.cnpq.br/2746554350570335
 http://escolaarcadia.blogspot.com.br/
+https://github.com/martinmoraes
+
+Este código está disponível em: https://github.com/martinmoraes/Android-AbrindoActivity
  */
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import br.com.escolaarcadia.android_abrindoactivity.R;
 
@@ -18,5 +24,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void abrirSegundaActivity(View view){
+        Intent intent = new Intent(this, SegundaActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void abrirNavegador(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com.br"));
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/br/com/blogspot/escolaarcadia/SegundaActivity.java
@@ -0,0 +1,15 @@
+package br.com.blogspot.escolaarcadia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.escolaarcadia.android_abrindoactivity.R;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
